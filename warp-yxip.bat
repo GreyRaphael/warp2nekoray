704ff1b85e18a3c236102be7e6e2582407556ebd
BeGeWei <grey@pku.edu.cn>
2023-10-22 00:44:08 +0800

    add depends

---
+++ warp-yxip.bat
@@ -0,0 +1,35 @@
+cls
+@echo off & setlocal enabledelayedexpansion
+
+set filename=ips-v4.txt & goto getv4
+
+:getv4
+for /f "delims=" %%i in (%filename%) do (
+set !random!_%%i=randomsort
+)
+for /f "tokens=2,3,4 delims=_.=" %%i in ('set ^| findstr =randomsort ^| sort /m 10240') do (
+call :randomcidrv4
+if not defined %%i.%%j.%%k.!cidr! set %%i.%%j.%%k.!cidr!=anycastip&set /a n+=1
+if !n! EQU 100 goto getip
+)
+goto getv4
+
+:randomcidrv4
+set /a cidr=%random%%%256
+goto :eof
+
+:getip
+del ip.txt > nul 2>&1
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =randomsort') do (
+set %%i=
+)
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =anycastip') do (
+echo %%i>>ip.txt
+)
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =anycastip') do (
+set %%i=
+)
+
+warp
+del ip.txt > nul 2>&1
+exit
